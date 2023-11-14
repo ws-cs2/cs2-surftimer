4ef82652bc60b514d04dd8282b278c5c8b6b3970
ws <>
2023-11-14 21:57:26 +1300

    Add another spec hearing command

--- scripts/vscripts/wst/wst-cvars.lua
+++ scripts/vscripts/wst/wst-cvars.lua
@@ -34,6 +34,7 @@ function SurfCVars()
     SendToServerConsole("sv_full_alltalk 1")
     SendToServerConsole("sv_alltalk 1")
     SendToServerConsole("sv_allchat 1")
+    SendToServerConsole("sv_spec_hear 1")
 
     -- Allow time for the cs2 vote map thing
     SendToServerConsole("mp_round_restart_delay 15")
